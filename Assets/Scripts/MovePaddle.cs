6a9036bda3e9ef5a66ef0e2797debb30e73b00d7
Doug Linley <spacemunkee.codes@gmail.com>
2017-03-11 21:56:04 -0500

    Added paddle and paddle movement

---
+++ Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour {
+
+	public float speed = 6f;
+	float paddleAdjustment = 0.35f;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+		position += new Vector3(Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0, 0);
+
+		float screenRatio = (float)Screen.width / (float)Screen.height;
+		float widthOrtho = Camera.main.orthographicSize * screenRatio;
+
+		if (position.x + paddleAdjustment > widthOrtho) {
+			position.x = widthOrtho - paddleAdjustment;
+		}
+
+		if (position.x - paddleAdjustment < -widthOrtho) {
+			position.x = -widthOrtho + paddleAdjustment;
+		}
+
+		transform.position = position;
+	}
+}
